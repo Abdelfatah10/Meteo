2100651a9fae0e94ee9be3b54139b6255e3740c5
Abdelfatah10 <djaballahabdelfatah9@gmail.com>
2025-07-02 14:07:37 +0100

    Fix meteo.html path or move to public

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'meteo.html'));
+    res.sendFile(path.join(__dirname, 'meteo.html'));
 });
 const ipGeoKey = process.env.IPGEO_API_KEY ;
 
